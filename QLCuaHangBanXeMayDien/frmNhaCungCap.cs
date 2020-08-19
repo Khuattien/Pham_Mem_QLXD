a936b1aee41a8504c74759c1c1aa227206ad3a78
hunglovegame <phamthehung2212000@gmail.com>
2020-08-19 23:36:27 +0700

    ywrt

--- QLCuaHangBanXeMayDien/frmNhaCungCap.cs
+++ QLCuaHangBanXeMayDien/frmNhaCungCap.cs
@@ -16,7 +16,7 @@ namespace QLCuaHangBanXeMayDien
         {
             InitializeComponent();
             //tôi là hùng
-            //lần cuối nữa thôi
+            //lần cuối nữa thôirêtrtểtrtrêtr45ẻ54ty34
         }
 
     }
